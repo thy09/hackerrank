73d678cc8a5ff48d1825c43f3cd4f2bfb61233c3
unknown <tangheyi.09@gmail.com>
2014-09-03 15:29:40 +0800

    project_euler initial

---
+++ proj_euler/t1_mul35.hs
@@ -0,0 +1,19 @@
+main = do
+    count <- getLine
+    lines <- getLines (read count)
+    let nums = map (\l -> read l :: Int) lines
+    sequence (map (\num -> print (mAorB (num - 1) 3 5)) nums)
+
+getLines :: Int -> IO [String]
+getLines count = do
+  sequence (take count (repeat getLine))
+  
+mAorB :: Int -> Int -> Int -> Int
+mAorB num a b = 
+	let lcmab = lcm a b
+	in (divsum num a) - (divsum num lcmab) + (divsum num b)
+
+divsum :: Int -> Int -> Int
+divsum num k = 
+	let d = num `div` k
+	in ((1+d)*d `div` 2)*k
